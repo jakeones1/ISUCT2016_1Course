20e79c7aed11979b714be702c1013223bdbc107b
BPavelS <asdasddddqwerty@gmail.com>
2016-11-28 10:59:59 +0400

    11

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@
  * Created by stud_6 on 28.11.16.
  */
 public class Demo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Breakk Demo = new Breakk();
         Demo.Warm();
     }
