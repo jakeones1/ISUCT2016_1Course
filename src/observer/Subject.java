ed03e8fcf756953ac5e4dfc4dd6685d819e4be91
sergey <jakeones11@gmail.com>
2017-10-23 10:33:33 +0300

    observer

---
+++ src/observer/Subject.java
@@ -0,0 +1,11 @@
+package observer;
+
+/**
+ * Created by UI on 23.10.2017.
+ */
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+
+}
