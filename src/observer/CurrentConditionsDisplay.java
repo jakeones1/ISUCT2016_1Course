39084cdbd931cfe7ccb863e1d387d3a625ada5d9
Sergey <jakeones1>
2017-11-06 10:42:26 +0300

    lev durak

--- src/observer/CurrentConditionsDisplay.java
+++ src/observer/CurrentConditionsDisplay.java
@@ -1,22 +1,30 @@
 package observer;
 
+import java.util.Observer;
+import java.util.Observable;
+
+
+
 /**
  * Created by UI on 23.10.2017.
  */
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    Observable observable;
     private float temperature;
     private float humidity;
-    private Subject weatherData;
 
-    public CurrentConditionsDisplay(Subject weatherData){
-        this.weatherData = weatherData;
-        weatherData.registerObserver(this);
+    public CurrentConditionsDisplay(Observable observable){
+        this.observable = observable;
+        observable.addObserver(this);
     }
 
-    public void update(float temperature,float humidity,float pressure){
-        this.temperature = temperature;
-        this.humidity = humidity;
-        display();
+    public void update(Observable obs, Object arg){
+        if (obs instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData)obs;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
     }
 
     public void display(){
