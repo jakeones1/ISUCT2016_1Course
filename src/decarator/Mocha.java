c96715073de0cd5aed7bc915cbf5a41100aea3d1
Sergey <jakeones1>
2017-12-04 10:48:22 +0300

    string format

---
+++ src/decarator/Mocha.java
@@ -0,0 +1,22 @@
+package decarator;
+
+/**
+ * Created by UI on 22.11.2017.
+ */
+public class Mocha extends CondimentDecorator {
+    Beverage beverage;
+
+    public Mocha(Beverage beverage) {
+        this.beverage=beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Mocha";
+    }
+
+    @Override
+    public double cost() {
+        return .20 + beverage.cost();
+    }
+}
